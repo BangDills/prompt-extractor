231db21e5ac50c0cb7280cf2f7be680eea1b5113
Pinokioarab <pinokioarab@gmail.com>
2026-06-22 19:07:25 +0700

    test: delete test file scripts/hello.js

--- scripts/hello.js
+++
@@ -1,9 +0,0 @@
-// Test file untuk sandbox tool testing
-console.log("Hello from scripts/hello.js!");
-console.log("Tools testing berhasil! 🎉");
-
-function greet(name) {
-  return `Halo, ${name}! Selamat datang di Image to Prompt.`;
-}
-
-module.exports = { greet };
